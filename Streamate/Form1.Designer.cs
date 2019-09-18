f71d58f04b6f8d21c1cb6f1f65747f0a4c28612a
Bytes Crafter <bytescrafter@gmail.com>
2019-09-18 20:59:02 +0800

    Added system tray menu and mechanism.

--- Streamate/Form1.Designer.cs
+++ Streamate/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Streamate
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Required designer variable.
@@ -29,26 +29,135 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.streamateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facebookToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.youTubeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.websiteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facebookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.youTubeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.websiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // notifyIcon1
             // 
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "If you need me, you can find me here on your system tray.";
+            this.notifyIcon1.BalloonTipTitle = "Streamate";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Streamate";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
-            // Form1
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.streamateToolStripMenuItem,
+            this.optionToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 120);
+            this.contextMenuStrip1.Closed += new System.Windows.Forms.ToolStripDropDownClosedEventHandler(this.contextMenuStrip1_Closed);
+            // 
+            // streamateToolStripMenuItem
+            // 
+            this.streamateToolStripMenuItem.Enabled = false;
+            this.streamateToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.streamateToolStripMenuItem.Name = "streamateToolStripMenuItem";
+            this.streamateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.streamateToolStripMenuItem.Text = "Streamate";
+            this.streamateToolStripMenuItem.ToolTipText = "Thank You!";
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.optionToolStripMenuItem.Text = "Option";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.facebookToolStripMenuItem1,
+            this.youTubeToolStripMenuItem1,
+            this.websiteToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // facebookToolStripMenuItem1
+            // 
+            this.facebookToolStripMenuItem1.Name = "facebookToolStripMenuItem1";
+            this.facebookToolStripMenuItem1.Size = new System.Drawing.Size(125, 22);
+            this.facebookToolStripMenuItem1.Text = "Facebook";
+            this.facebookToolStripMenuItem1.Click += new System.EventHandler(this.facebookToolStripMenuItem1_Click);
+            // 
+            // youTubeToolStripMenuItem1
+            // 
+            this.youTubeToolStripMenuItem1.Name = "youTubeToolStripMenuItem1";
+            this.youTubeToolStripMenuItem1.Size = new System.Drawing.Size(125, 22);
+            this.youTubeToolStripMenuItem1.Text = "YouTube";
+            this.youTubeToolStripMenuItem1.Click += new System.EventHandler(this.youTubeToolStripMenuItem1_Click);
+            // 
+            // websiteToolStripMenuItem1
+            // 
+            this.websiteToolStripMenuItem1.Name = "websiteToolStripMenuItem1";
+            this.websiteToolStripMenuItem1.Size = new System.Drawing.Size(125, 22);
+            this.websiteToolStripMenuItem1.Text = "Website";
+            this.websiteToolStripMenuItem1.Click += new System.EventHandler(this.websiteToolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // facebookToolStripMenuItem
+            // 
+            this.facebookToolStripMenuItem.Name = "facebookToolStripMenuItem";
+            this.facebookToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // youTubeToolStripMenuItem
+            // 
+            this.youTubeToolStripMenuItem.Name = "youTubeToolStripMenuItem";
+            this.youTubeToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // websiteToolStripMenuItem
+            // 
+            this.websiteToolStripMenuItem.Name = "websiteToolStripMenuItem";
+            this.websiteToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(533, 384);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.MaximizeBox = false;
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " Streamate by Bytes Crafter";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Menu_FormClosed);
             this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +165,18 @@
         #endregion
 
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facebookToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem youTubeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem websiteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facebookToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem youTubeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem websiteToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem streamateToolStripMenuItem;
     }
 }
 
--- Streamate/Form1.cs
+++ Streamate/Form1.cs
@@ -10,18 +10,21 @@ using System.Windows.Forms;
 
 namespace Streamate
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
 
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            notifyIcon1.Visible = false;
-            this.ShowInTaskbar = true;
-            WindowState = FormWindowState.Normal;
+            if (!this.ShowInTaskbar)
+            {
+                //notifyIcon1.Visible = false;
+                this.ShowInTaskbar = true;
+                WindowState = FormWindowState.Normal;
+            }
         }
 
         private void Form1_SizeChanged(object sender, EventArgs e)
@@ -29,28 +32,57 @@ namespace Streamate
             bool mousePointerNotOnTaskbar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
             if( this.WindowState == FormWindowState.Minimized && mousePointerNotOnTaskbar)
             {
-                notifyIcon1.Icon = SystemIcons.Application;
-                //notifyIcon1.Icon = ;
-                notifyIcon1.BalloonTipClicked += new System.EventHandler(NotifyIcon1_Click);
-                notifyIcon1.BalloonTipTitle = "Streamate";
-                notifyIcon1.BalloonTipText = "Im just here!";
+                notifyIcon1.BalloonTipClicked += new System.EventHandler(notifyIcon2_DoubleClick);
                 notifyIcon1.ShowBalloonTip(1000);
                 this.ShowInTaskbar = false;
-                notifyIcon1.Visible = true;
+                //Streamate.Visible = true;
             }
-            
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void notifyIcon2_DoubleClick(object sender, EventArgs e)
+        {
+            //Yes: Called when ballon is clicked!
+            //Bug: Called when right click system tray icon.
+        }
+
+
+
+        private void facebookToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://facebook.com/BytesCrafterPH");
+        }
+
+        private void youTubeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://youtube.com/channel/UCtszwIubJejX-nndEBE-rhw");
+        }
+
+        private void websiteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://bytes-crafter.com");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void contextMenuStrip1_Closed(object sender, ToolStripDropDownClosedEventArgs e)
+        {
+            streamateToolStripMenuItem_Click(this, null);
+        }
+
+        private void streamateToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void NotifyIcon1_Click(object sender, System.EventArgs e)
+        private void Menu_FormClosed(object sender, FormClosedEventArgs e)
         {
-            notifyIcon1.Visible = false;
-            this.ShowInTaskbar = true;
-            WindowState = FormWindowState.Normal;
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                //Instead of exiting when close button on form is clicked!
+            }
         }
     }
 }
